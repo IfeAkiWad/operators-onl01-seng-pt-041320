1ac8bbb5ae470ff3166185a90d584e62ebac5ec7
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-16 04:53:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,21 @@
 # require "pry"
 
-def unsafe?(speed)
+# def unsafe?(speed)
 
   
 
-end
+# end
 
-def not_safe?(speed)
+# def not_safe?(speed)
 	
+# end
+def unsafe?(speed)
+if speed > 60 || speed < 40
+   true
+else 
+  false
+  end
+
 end
-	
 
 
